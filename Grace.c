af8f0a4acecc18ffa4bd9d0d0a33b39484dda990
Nathan <jcarra@student.42.fr>
2018-03-28 18:40:03 +0200

    Grace ok

---
+++ Grace.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+/*
+	Ceci est un commentaire hors des fonctions.
+*/
+#define FT(x)int main(){FILE *f=fopen("Grace_kid.c", "w+");char *c="#include <stdio.h>%c%c/*%c%cCeci est un commentaire hors des fonctions.%c*/%c#define FT(x)int main(){FILE *f=fopen(%cGrace_kid.c%c, %cw+%c);char *c=%c%s%c;fprintf(f,c,10,10,10,9,10,10,34,34,34,34,34,c,34,10,10,10);return (0);}%c%cFT(xxxxxxx)%c";fprintf(f,c,10,10,10,9,10,10,34,34,34,34,34,c,34,10,10,10);return (0);}
+
+FT(xxxxxxx)
