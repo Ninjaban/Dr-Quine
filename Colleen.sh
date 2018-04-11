c8c703ac8c54b3b1ed7591cbeb11aaa25ddbc679
Jonathan Carra <jcarra@student.42.fr>
2018-04-11 16:43:38 +0200

    Update ok

--- Colleen.sh
+++ Colleen.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-ft_function() {}
+ft_function() {
+a=0;}
 
+#Un commentaire
 main()
 {
 #Fonction sans interret
 ft_function
 
-b=\' c=\\ a='/bin/echo "#!/bin/sh" ; /bin/echo ; /bin/echo "ft_function() {}" ; /bin/echo ; /bin/echo "main()" ; /bin/echo "{" ; /bin/echo "#Fonction sans interret" ; /bin/echo "ft_function" ; /bin/echo ; /bin/echo b=$c$b c=$c$c a=$b$a$b; /bin/echo $a ; /bin/echo "}" ; /bin/echo ; /bin/echo "main"'
-/bin/echo "#!/bin/sh" ; /bin/echo ; /bin/echo "ft_function() {}" ; /bin/echo ; /bin/echo "main()" ; /bin/echo "{" ; /bin/echo "#Fonction sans interret" ; /bin/echo "ft_function" ; /bin/echo ; /bin/echo b=$c$b c=$c$c a=$b$a$b; /bin/echo $a ; /bin/echo "}" ; /bin/echo ; /bin/echo "main"
+b=\' c=\\ a='/bin/echo "#!/bin/sh" ; /bin/echo ; /bin/echo "ft_function() {" ; /bin/echo "a=0;}" ; /bin/echo ; /bin/echo "#Un commentaire" ; /bin/echo "main()" ; /bin/echo "{" ; /bin/echo "#Fonction sans interret" ; /bin/echo "ft_function" ; /bin/echo ; /bin/echo b=$c$b c=$c$c a=$b$a$b; /bin/echo $a ; /bin/echo "}" ; /bin/echo ; /bin/echo "main"'
+/bin/echo "#!/bin/sh" ; /bin/echo ; /bin/echo "ft_function() {" ; /bin/echo "a=0;}" ; /bin/echo ; /bin/echo "#Un commentaire" ; /bin/echo "main()" ; /bin/echo "{" ; /bin/echo "#Fonction sans interret" ; /bin/echo "ft_function" ; /bin/echo ; /bin/echo b=$c$b c=$c$c a=$b$a$b; /bin/echo $a ; /bin/echo "}" ; /bin/echo ; /bin/echo "main"
 }
 
 main
--- Grace.s
+++ Grace.s
@@ -1,5 +1,5 @@
 section .data
-    fmt db 'section .data%c    fmt db %c%s%c, 0%c    file db %cGrace_kid.s%c, 0%c    mode db %cw+%c, 0%c%csection .text%c    global start%c    global _main%c    extern _fopen%c    extern _fprintf%c    extern _fclose%c%c%%define TRUE 1%c%%define FALSE 0%c%c; c est ici que tout se passe%c%%macro FT 1%c_main:%c    push rbp%c    mov rbp, rsp                ; Initialise la stack pour la fonction%c%c    sub rsp, 0x270              ; Ajoute de la stack (multiple de 16 obligatoire !)%c%c    ; fopen%c    lea rdi, [rel file]         ; fopen path%c    lea rsi, [rel mode]         ; fopen mode%c%c    call _fopen                 ; Call fopen%c%c    ; fprintf%c    mov rdi, rax                ; printf file%c    lea rsi, [rel fmt]          ; printf format%c%c    ; On utilise les registres pour les premiers args%c    mov rdx, 0xa                ; printf "\n"%c    mov rcx, 0x27               ; printf Quote%c    lea r8, [rel fmt]           ; printf fmt%c    mov r9, 0x27                ; printf Quote%c%c    ; Plus de registre disponible, on utilise la stack pour les prochains args%c    mov word [rsp+0x00], 0xa    ; printf "\n"%c    mov word [rsp+0x08], 0x27   ; printf Quote%c    mov word [rsp+0x10], 0x27   ; printf Quote%c    mov word [rsp+0x18], 0xa    ; printf "\n"%c    mov word [rsp+0x20], 0x27   ; printf Quote%c    mov word [rsp+0x28], 0x27   ; printf Quote%c%c    mov r10, 0x30               ; Set r10 a 0x30%c    jmp _set_stack              ; va a set_stack%c%c_set_stack:%c    mov word [rsp+r10], 0xa     ; printf "\n"%c    add r10, 0x8                ; r10 += 8%c    cmp r10, 0x260              ; Boucle jusqu a ce que toute la stack soit remplie%c    jl _set_stack%c    jmp _call_fprintf           ; Fin de boucle, va a call_printf%c%c_call_fprintf:%c    mov [rsp+0x260], rdi        ; Sauve rdi sur la stack%c    call _fprintf               ; Call printf%c    mov rdi, [rsp+0x260]        ; Charge rdi depuis la stack%c    call _fclose                ; Call fclose%c%c    add rsp, 0x270              ; Rend la stack%c    xor rax, rax                ; Set rax a 0%c%c    leave                       ; reset la stack%c    ret                         ; return (rax);%c%%endmacro%c%cstart:%c    FT xxxxxx%c', 0
+    fmt db 'section .data%c    fmt db %c%s%c, 0%c    file db %cGrace_kid.s%c, 0%c    mode db %cw+%c, 0%c%csection .text%c    global start%c    global _main%c    extern _fopen%c    extern _fprintf%c    extern _fclose%c%c%%define TRUE 1%c%%define FALSE 0%c%%define ERROR 2%c%c; c est ici que tout se passe%c%%macro FT 1%c_main:%c    push rbp%c    mov rbp, rsp%c%c    sub rsp, 0x270%c%c    lea rdi, [rel file]%c    lea rsi, [rel mode]%c%c    call _fopen%c%c    mov rdi, rax%c    lea rsi, [rel fmt]%c%c    mov rdx, 0xa%c    mov rcx, 0x27%c    lea r8, [rel fmt]%c    mov r9, 0x27%c%c    mov word [rsp+0x00], 0xa%c    mov word [rsp+0x08], 0x27%c    mov word [rsp+0x10], 0x27%c    mov word [rsp+0x18], 0xa%c    mov word [rsp+0x20], 0x27%c    mov word [rsp+0x28], 0x27%c%c    mov r10, 0x30%c    jmp _set_stack%c%c_set_stack:%c    mov word [rsp+r10], 0xa%c    add r10, 0x8%c    cmp r10, 0x260%c    jl _set_stack%c    jmp _call_fprintf%c%c_call_fprintf:%c    mov [rsp+0x260], rdi%c    call _fprintf%c    mov rdi, [rsp+0x260]%c    call _fclose%c%c    add rsp, 0x270%c    xor rax, rax%c%c    leave%c    ret%c%%endmacro%c%cstart:%c    FT xxxxxx%c', 0
     file db 'Grace_kid.s', 0
     mode db 'w+', 0
 
@@ -12,60 +12,57 @@ section .text
 
 %define TRUE 1
 %define FALSE 0
+%define ERROR 2
 
 ; c est ici que tout se passe
 %macro FT 1
 _main:
     push rbp
-    mov rbp, rsp                ; Initialise la stack pour la fonction
+    mov rbp, rsp
 
-    sub rsp, 0x270              ; Ajoute de la stack (multiple de 16 obligatoire !)
+    sub rsp, 0x270
 
-    ; fopen
-    lea rdi, [rel file]         ; fopen path
-    lea rsi, [rel mode]         ; fopen mode
+    lea rdi, [rel file]
+    lea rsi, [rel mode]
 
-    call _fopen                 ; Call fopen
+    call _fopen
 
-    ; fprintf
-    mov rdi, rax                ; printf file
-    lea rsi, [rel fmt]          ; printf format
+    mov rdi, rax
+    lea rsi, [rel fmt]
 
-    ; On utilise les registres pour les premiers args
-    mov rdx, 0xa                ; printf "\n"
-    mov rcx, 0x27               ; printf Quote
-    lea r8, [rel fmt]           ; printf fmt
-    mov r9, 0x27                ; printf Quote
+    mov rdx, 0xa
+    mov rcx, 0x27
+    lea r8, [rel fmt]
+    mov r9, 0x27
 
-    ; Plus de registre disponible, on utilise la stack pour les prochains args
-    mov word [rsp+0x00], 0xa    ; printf "\n"
-    mov word [rsp+0x08], 0x27   ; printf Quote
-    mov word [rsp+0x10], 0x27   ; printf Quote
-    mov word [rsp+0x18], 0xa    ; printf "\n"
-    mov word [rsp+0x20], 0x27   ; printf Quote
-    mov word [rsp+0x28], 0x27   ; printf Quote
+    mov word [rsp+0x00], 0xa
+    mov word [rsp+0x08], 0x27
+    mov word [rsp+0x10], 0x27
+    mov word [rsp+0x18], 0xa
+    mov word [rsp+0x20], 0x27
+    mov word [rsp+0x28], 0x27
 
-    mov r10, 0x30               ; Set r10 a 0x30
-    jmp _set_stack              ; va a set_stack
+    mov r10, 0x30
+    jmp _set_stack
 
 _set_stack:
-    mov word [rsp+r10], 0xa     ; printf "\n"
-    add r10, 0x8                ; r10 += 8
-    cmp r10, 0x260              ; Boucle jusqu a ce que toute la stack soit remplie
+    mov word [rsp+r10], 0xa
+    add r10, 0x8
+    cmp r10, 0x260
     jl _set_stack
-    jmp _call_fprintf           ; Fin de boucle, va a call_printf
+    jmp _call_fprintf
 
 _call_fprintf:
-    mov [rsp+0x260], rdi        ; Sauve rdi sur la stack
-    call _fprintf               ; Call printf
-    mov rdi, [rsp+0x260]        ; Charge rdi depuis la stack
-    call _fclose                ; Call fclose
+    mov [rsp+0x260], rdi
+    call _fprintf
+    mov rdi, [rsp+0x260]
+    call _fclose
 
-    add rsp, 0x270              ; Rend la stack
-    xor rax, rax                ; Set rax a 0
+    add rsp, 0x270
+    xor rax, rax
 
-    leave                       ; reset la stack
-    ret                         ; return (rax);
+    leave
+    ret
 %endmacro
 
 start:
